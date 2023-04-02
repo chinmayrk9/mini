2a4c494cbca7704f9c551388547c1d6094e7008d
chinmayrk9 <66291892+chinmayrk9@users.noreply.github.com>
2023-04-02 07:45:21 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 public class Main { private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        // Starting of calculator work.......
+        // Starting of calculator work.....
         logger.info("Starting the Calculator Application.");
 
         Scanner scanner = new Scanner(System.in);
